f2232b7fa5a5cc0f6a99491d991e81e5a022c95f
Jackson Bayor <42275320+jaxenvisuals@users.noreply.github.com>
2025-06-10 08:27:21 +0100

    Setup element plus and tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+  import { ElButton } from "element-plus";
+</script>
+
 <template>
-  <div class="font-bold">Hello</div>
+  <div class="font-bold text-3xl">Hello</div>
+  <div>
+    <ElButton class="test">Test</ElButton>
+  </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, type UserConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import ElementPlus from "unplugin-element-plus/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+export default defineConfig(() => {
+  const configurations: UserConfig = {
+    plugins: [
+      vue(),
+      ElementPlus({
+        useSource: true,
+      }),
+      tailwindcss(),
+    ],
+  };
+
+  return configurations;
 });
