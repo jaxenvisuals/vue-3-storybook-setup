4cb12a4218f3d6c85413c9b5d1f2aea39bed6ccf
Jackson Bayor <42275320+jaxenvisuals@users.noreply.github.com>
2025-07-03 00:18:09 +0100

    v0.2.7

--- dist/bayor-ui.es.js
+++ dist/bayor-ui.es.js
@@ -1,21 +1,23 @@
-import { defineComponent as t, createElementBlock as o, openBlock as r, createVNode as p, unref as n, withCtx as l, createTextVNode as a, toDisplayString as c } from "vue";
-import { ElButton as i } from "element-plus";
-const s = /* @__PURE__ */ t({
+import { defineComponent as o, createElementBlock as n, openBlock as l, createVNode as r, unref as p, withCtx as a, createTextVNode as i, createElementVNode as u, toDisplayString as m } from "vue";
+import { ElButton as s } from "element-plus";
+const _ = /* @__PURE__ */ o({
   __name: "AppButton",
   props: {
     label: { default: "Click Me!!" }
   },
-  setup(u) {
-    return (e, m) => (r(), o("div", null, [
-      p(n(i), { type: "primary" }, {
-        default: l(() => [
-          a(c(e.label), 1)
+  setup(d) {
+    return (t, e) => (l(), n("div", null, [
+      r(p(s), { type: "primary" }, {
+        default: a(() => [
+          i(m(t.label) + " ", 1),
+          e[0] || (e[0] = u("span", { class: "font-bold underline" }, "Hello", -1))
         ]),
-        _: 1
+        _: 1,
+        __: [0]
       })
     ]));
   }
 });
 export {
-  s as AppButton
+  _ as AppButton
 };
